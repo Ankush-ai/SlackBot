5fb493802e197aa0a8ba9f2537f2b673bde636ad
Ankush Srivastava <ankushsri007@gmail.com>
2025-05-17 23:28:58 +0530

    new commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { App } = require('@slack/bolt');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
 });
 
