e97bac116d5b1c692389a142d56ec3edfc246564
Ankush Srivastava <ankushsri007@gmail.com>
2025-05-17 23:18:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+
+// Slack app with Socket Mode enabled
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+// Slash command listener example
+app.command('/hello', async ({ command, ack, say }) => {
+  await ack();
+  await say(`Hello <@${command.user_id}>!`);
+});
+
+// Start your app
+(async () => {
+  await app.start();
+  console.log('⚡️ Slack app is running with Socket Mode!');
+})();
